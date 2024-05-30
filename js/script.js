5c79ec1b96155031e25379e3c95c07a90aa66643
6valencia <ignaciovalencia002@gmail.com>
2024-05-30 13:38:20 +0200

    comentarios y tal

--- js/script.js
+++ js/script.js
@@ -14,6 +14,6 @@ const appendAlert = (message, type) => {
 const alertTrigger = document.getElementById('liveAlertBtn')
 if (alertTrigger) {
   alertTrigger.addEventListener('click', () => {
-    appendAlert('Nice, you triggered this alert message!', 'success')
+    appendAlert('Tu comentario ha sido enviado correctamente!', 'success')
   })
 }
